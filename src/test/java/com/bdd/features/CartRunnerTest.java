14433a97e7c3af68d95c402f6d1eedcee37ac9ed
kalpa <kalpa@Kalpana.mynetworksettings.com>
2026-03-03 22:03:37 -0500

    I commit

--- src/test/java/com/bdd/features/CartRunnerTest.java
+++ src/test/java/com/bdd/features/CartRunnerTest.java
@@ -6,8 +6,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/cart/cart.feature",glue={"com.steps"})
-
+@CucumberOptions(features="src/test/resources/cart/cart.feature",glue={"com.steps"},
+tags="@smoke")
 public class CartRunnerTest {
 	
 	
