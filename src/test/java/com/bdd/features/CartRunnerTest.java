84164e71aff8beb1c685124993eca227f631665e
kalpa <kalpa@Kalpana.mynetworksettings.com>
2026-03-03 22:52:41 -0500

    I commit

--- src/test/java/com/bdd/features/CartRunnerTest.java
+++ src/test/java/com/bdd/features/CartRunnerTest.java
@@ -1,5 +1,7 @@
 package com.bdd.features;
 
+
+
 import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
@@ -9,7 +11,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions(features="src/test/resources/cart/cart.feature",glue={"com.steps"},
 tags="@smoke",
 plugin= {"pretty","html:target/cucumber-report.html"})
-public class CartRunnerTest {
+public class CartRunnerTest{
 	
 	
 
--- src/test/java/com/steps/shoppingCart.java
+++ src/test/java/com/steps/shoppingCart.java
@@ -1,15 +1,16 @@
 package com.steps;
-
 import java.time.Duration;
-
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.cucumber.java.en.Given;
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.Assert;
-
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
